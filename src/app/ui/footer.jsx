b230e45a4a4d57e3de0e5c6ccbe69a38d9cf4bca
frigvid <67565373+frigvid@users.noreply.github.com>
2024-01-22 23:03:36 +0100

    chore: Fix some spacing, add back a CSS rule that took vacation.

--- src/app/ui/footer.jsx
+++ src/app/ui/footer.jsx
@@ -1,8 +1,6 @@
-
-
 export default function Footer() {
     return (
-        <footer className=" bg-main">
+        <footer className="bg-main p-4">
             <p className="text-center text-white">© 2024 Chess Buddy. All rights reserved.</p>
         </footer>
     )
