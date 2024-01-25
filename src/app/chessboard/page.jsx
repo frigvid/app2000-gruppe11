145aac698c9e23c649c3e8a5b479155036dcb808
Jarle Olsen <33558641+jarle0@users.noreply.github.com>
2024-01-25 16:56:41 +0100

    feat: updated file with simpler code .

--- src/app/chessboard/page.jsx
+++ src/app/chessboard/page.jsx
@@ -2,10 +2,10 @@
 import React from 'react';
 
 // Importing the 'Arena' component from the specified file path.
-import Arena from '../ui/chessboard/arena';
+import Arena from '../ui/chessboard/chessboard.jsx';
 
 // Defining the 'App' functional component.
-const App = () => {
+const Home = () => {
 	// Returning JSX (JavaScript XML) to define the structure of the component.
 	return (
 		// Using a <div> element with CSS classes to center the content both horizontally and vertically.
@@ -17,4 +17,4 @@ const App = () => {
 };
 
 // Exporting the 'App' component as the default export of this module.
-export default App;
+export default Home;
