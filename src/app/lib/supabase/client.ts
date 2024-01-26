ef6ca30ae4c904e9ab6282e77d3b831b20bdcc69
frigvid <67565373+frigvid@users.noreply.github.com>
2024-01-26 18:05:14 +0100

    fix: Remove unnecessary 'use client' call, and extraneous comma.

--- src/app/lib/supabase/client.ts
+++ src/app/lib/supabase/client.ts
@@ -1,9 +1,7 @@
-"use client";
-
-import {createBrowserClient} from "@supabase/ssr";
+import {createBrowserClient} from '@supabase/ssr';
 
 export const createClient = () =>
 	createBrowserClient(
 		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 	);
