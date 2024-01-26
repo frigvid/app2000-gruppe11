ad0bfe4081e73273a00e58d4569fecb195455c6e
frigvid <67565373+frigvid@users.noreply.github.com>
2024-01-26 18:06:49 +0100

    refactor: Make header component use @ui alias.

--- src/app/ui/header.jsx
+++ src/app/ui/header.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import logoIcon from "/public/logo.svg";
-import Login from "./login/login";
+import Login from "@ui/auth/login";
 
 export default function Header() {
 	return (
