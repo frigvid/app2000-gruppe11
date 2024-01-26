3b7c40792ef35990452a778219077ab328bc5a28
frigvid <67565373+frigvid@users.noreply.github.com>
2024-01-26 18:10:54 +0100

    feat: Added email notification to user-email if user has forgotten their password.

---
+++ src/app/auth/forgot/page.tsx
@@ -0,0 +1,81 @@
+import {createClient} from "@lib/supabase/server";
+//import {redirect} from "next/navigation";
+import {cookies} from "next/headers";
+import Link from "next/link";
+
+export default function Forgot() {
+	const resetPasswordEmail = async (formData: FormData) => {
+		"use server";
+
+		const email = formData.get("email") as string;
+		const cookieStore = cookies();
+		const supabase = createClient(cookieStore);
+
+		const { data, error } = await supabase.auth
+			.resetPasswordForEmail(email)
+
+		if (error) {
+			/* See note below. */
+			//return redirect("/auth/forgot?message=Something went wrong");
+		}
+
+		/* While it /could/ be nice to have a success message, the problem is as
+		 * always security. Doing this, you could theoretically find users if you
+		 * brute-force the process by simply trying different emails.
+		 *
+		 * There are ways to minimize these things, such as rules that only allow
+		 * a certain amount of tries per IP and increasing timeouts. But it's just
+		 * a countermeasure. Automating that process would make this trivial, if
+		 * very slow, to do.
+		 *
+		 * Though that's no guarantee for a success, I feel I might as well leave
+		 * no message unless something went wrong. /However/, that means another
+		 * avenue through which to guess which emails /are/ in the database,
+		 * because it'll only return an error on a miss. Thus, both are disabled.
+		 */
+		//return redirect("/auth/forgot?message=Check email to continue sign in process");
+	};
+
+	return (
+		<main className="flex justify-center items-center">
+			<div className="bg-white p-4 rounded shadow-lg">
+				<form action={resetPasswordEmail}>
+					<div className="mb-4">
+						<label
+							className="block text-gray-700 text-sm font-bold mb-2"
+							htmlFor="email"
+						>
+							Email Address
+						</label>
+						<input
+							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							id="email"
+							type="text"
+							name="email"
+							required
+						/>
+					</div>
+					<div className="mb-2 pb-1 pt-1 text-center">
+						<button
+							className="bg-buttoncolor mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal"
+							type="submit"
+						>
+							Reset Password
+						</button>
+					</div>
+				</form>
+				<div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+					<p className="mx-4 mb-0 text-center font-semibold">Or</p>
+				</div>
+				<div className="pb-1 pt-1 text-center">
+					<Link
+						className="bg-buttoncolor mb-3 inline-block rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal"
+						href="/auth/signin"
+					>
+						Back to log in
+					</Link>
+				</div>
+			</div>
+		</main>
+	);
+}
