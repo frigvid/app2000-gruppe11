38ad0a74bf2d07bebc947ee8d81150e0fbe3cc00
frigvid <67565373+frigvid@users.noreply.github.com>
2024-01-30 15:42:46 +0100

    feat: Implemented basic buttons to enable user to go to chess page.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,30 @@
+import Link from "next/link";
+
 export default function Home() {
 	return (
-		<main>
-			<p>Home</p>
+		<main className="flex justify-center items-center">
+			<div className="flex flex-col space-y-4">
+				{/* TODO: When implemented, change bg-gray-500 to bg-blue-500. */}
+				<Link
+					className="block bg-gray-500 text-white text-center py-4 px-8 rounded"
+					href="/tren"
+				>
+					Tren (Not implemented)
+				</Link>
+				<Link
+					className="block bg-green-500 text-white text-center py-4 px-8 rounded"
+					href="/chess"
+				>
+					Spill
+				</Link>
+				{/* TODO: When implemented, change bg-gray-500 to bg-red-500. */}
+				<Link
+					className="block bg-gray-500 text-white text-center py-4 px-8 rounded"
+					href="/historikk"
+				>
+					Historikk (Not implemented)
+				</Link>
+			</div>
 		</main>
 	)
 }
