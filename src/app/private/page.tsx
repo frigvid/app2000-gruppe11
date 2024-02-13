9dad6aefda3ea5da8e523e9b0fe4b1d918bc99e6
frigvid <67565373+frigvid@users.noreply.github.com>
2024-02-13 20:59:29 +0100

    feat: Add temporary sign-up page, and protected route.

---
+++ src/app/private/page.tsx
@@ -0,0 +1,15 @@
+import { createClient } from '@lib/supabase/server'
+import { redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
+
+export default async function PrivatePage() {
+	const cookieStore = cookies()
+	const supabase = createClient(cookieStore)
+	
+	const { data, error } = await supabase.auth.getUser()
+	if (error || !data?.user) {
+		redirect('/')
+	}
+	
+	return <p>Hello {data.user.email}</p>
+}
