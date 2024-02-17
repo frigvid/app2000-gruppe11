c6aceb8f9fc8363686b66a0664592e2da16be29a
frigvid <67565373+frigvid@users.noreply.github.com>
2024-02-17 13:17:02 +0100

    fix: Change News page function name from App to News.

--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -1,9 +1,4 @@
-/**
- * Stub page for news.
- *
- * @author frigvid
- */
-export default function App() {
+export default function News() {
 	return (
 		<main className="flex justify-center items-center">
 			<p>News</p>
