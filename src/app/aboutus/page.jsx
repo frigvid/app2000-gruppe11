280843db058046167acd9211eef75f1b0ae6d7d7
frigvid <67565373+frigvid@users.noreply.github.com>
2024-02-27 20:14:01 +0100

    feat: Justify text, emphasize vision and reduce some CSS.

--- src/app/aboutus/page.jsx
+++ src/app/aboutus/page.jsx
@@ -8,28 +8,28 @@ import React from 'react';
  */
 export default function AboutUs() {
 	return (
-		<main className="flex flex-col items-center px-4 py-8">
-			<section className="max-w-4xl text-center space-y-4">
-				<h1 className="text-4xl font-bold mb-6">Velkommen til Chess Buddy</h1>
-				<p className="text-lg">
-					Her hos Chess Buddy, er vi drevet av en enkel visjon: Å gjøre sjakkundervisning av høy kvalitet
-					tilgjengelig for alle. Vår lidenskap for sjakk og en klar forståelse av markedets behov inspirerte oss
+		<main className="flex flex-col items-center text-center px-4 py-8">
+			<section className="max-w-4xl text-justify space-y-4 space-x-12 text-lg">
+				<h1 className="text-4xl text-center font-bold mb-6">Velkommen til Chess Buddy</h1>
+				<p>
+					Her hos Chess Buddy, er vi drevet av en enkel visjon: <em>Å gjøre sjakkundervisning av høy kvalitet
+					tilgjengelig for alle</em>. Vår lidenskap for sjakk og en klar forståelse av markedets behov inspirerte oss
 					til å skape et verktøy som spesifikt fokuserer på åpningstrekk; en vital, men ofte undervurdert, del av
 					spillet.
 				</p>
-				<p className="text-lg">
+				<p>
 					Vi anerkjente et gap i markedet for et dedikert verktøy som kunne tilby dypere innsikt og praktisk
 					trening i sjakkåpninger. Dette førte oss til å utvikle Chess Buddy, en plattform som kombinerer den
 					nyeste teknologien med omfattende sjakkekspertise for å tilby en unik læringsopplevelse.
 				</p>
-				<p className="text-lg">
+				<p>
 					Ved å utvikle Chess Buddy, ønsket vi å tilby et intuitivt og brukervennlig verktøy som demystifiserer
 					sjakkåpningene og gir spillere på alle nivåer muligheten til å forbedre sine åpningsstrategier og
 					generelle spill.
 				</p>
 			</section>
-			<section className="max-w-4xl mt-10 text-center space-y-4">
-				<h2 className="text-3xl font-bold mb-6">Vår Visjon</h2>
+			<section className="max-w-4xl mt-10 text-justify space-y-4 space-x-12">
+				<h2 className="text-3xl text-center font-bold mb-6">Vår Visjon</h2>
 				<p className="text-lg">
 					Vår visjon er å fremme sjakkundervisning ved å fokusere på åpningsstrategier. Med Chess Buddy strever vi
 					etter å utruste sjakkentusiaster med kunnskapen og ferdighetene de trenger for å heve spillet sitt. Vi er
