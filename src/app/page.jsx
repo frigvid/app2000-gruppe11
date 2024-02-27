c3fc563423a1db8c531c9fe77612aca71c266bbc
frigvid <67565373+frigvid@users.noreply.github.com>
2024-02-27 20:18:39 +0100

    feat: Make use of inheritance for text size.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -43,35 +43,35 @@ export default function Home() {
 					<FaArrowDown className="animate-bounce mx-auto text-2xl"/>
 				</div>
 			</div>
-			<div className="flex flex-col items-center px-4 py-8 mb-20">
+			<div className="flex flex-col items-center px-4 py-8 mb-20 text-lg">
 				<section className="max-w-4xl text-center space-y-4">
 					<h1 className="text-4xl font-bold text-center mb-6">Om Chess Buddy</h1>
-					<p className="text-lg text-center">
+					<p className="text-center">
 						Chess Buddy er en innovativ webapplikasjon designet for å styrke dine sjakkåpninger og forbedre ditt
 						generelle spill.
 					</p>
-					<p className="text-lg">
+					<p>
 						Ved å bruke Chess Buddy, kan spillere av alle nivåer utforske et bredt spekter av sjakkåpninger, lære
 						seg strategiene bak dem, og øve på å implementere dem i sine egne spill. Vår applikasjon er bygget med
 						en intuitiv forståelse for sjakkspilleres behov, og tilbyr personlig tilpassede læringsstier som gjør
 						det mulig for brukere å forbedre seg i sitt eget tempo.
 					</p>
-					<p className="text-lg">
+					<p>
 						Funksjoner inkluderer interaktive leksjoner, praktiske utfordringer, og muligheten til å spille mot en
 						AI for å teste ut nye åpninger i realistiske scenarier. Chess Buddy er ikke bare et verktøy for
 						læring; det er en partner i din sjakkutvikling.
 					</p>
-					<p className="text-lg">
+					<p>
 						Du kan også lese mer <Link className="text-xl font-semibold leading-6 hover:underline hover:underline-offset-8" href="/aboutus">om oss her</Link>.
 					</p>
 				</section>
 				<section className="max-w-4xl mt-10">
 					<h2 className="text-3xl font-bold text-center mb-6">Hvordan Chess Buddy Hjelper Deg</h2>
 					<ul className="list-disc space-y-2 pl-5">
-						<li className="text-lg">Tilpassede læringsstier som matcher ditt ferdighetsnivå og læringsmål.</li>
-						<li className="text-lg">Interaktive leksjoner som dyptgående forklarer hver åpning og dens variabler.</li>
-						<li className="text-lg">Utfordringer og spill mot AI for å praktisere og forsterke læringen.</li>
-						<li className="text-lg">Feedback og analyser som hjelper deg å forstå dine styrker og svakheter.</li>
+						<li>Tilpassede læringsstier som matcher ditt ferdighetsnivå og læringsmål.</li>
+						<li>Interaktive leksjoner som dyptgående forklarer hver åpning og dens variabler.</li>
+						<li>Utfordringer og spill mot AI for å praktisere og forsterke læringen.</li>
+						<li>Feedback og analyser som hjelper deg å forstå dine styrker og svakheter.</li>
 					</ul>
 				</section>
 			</div>
