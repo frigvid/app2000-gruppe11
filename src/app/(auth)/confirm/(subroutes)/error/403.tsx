f2b017e129f7f3e9757f857e6abd8b3fe940484e
frigvid <67565373+frigvid@users.noreply.github.com>
2024-04-04 00:47:10 +0200

    feat: Add subroute 403 error.

---
+++ src/app/(auth)/confirm/(subroutes)/error/403.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Link from "next/link";
+import ForbiddenError from "@ui/error/403_forbidden";
+
+/**
+ * If something goes wrong during the confirmation process,
+ * end-users are sent here.
+ *
+ * @author frigvid
+ * @created 2024-04-03
+ */
+export default function SignUpError() {
+	return (
+		<main className="flex flex-col justify-center items-center">
+			<div className="mb-8">
+				<ForbiddenError/>
+			</div>
+			<Link href="/"
+					className="bg-buttoncolor inline-block rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal"
+			>
+				Return to Home
+			</Link>
+		</main>
+	);
+}
