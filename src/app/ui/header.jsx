5c005ed08392f8b4740c0a2840c28fb94c3e2168
Jarle Olsen <33558641+jarle0@users.noreply.github.com>
2024-04-04 14:37:12 +0200

    featupdated the responsiveness to header with tailwindCSS.

--- src/app/ui/header.jsx
+++ src/app/ui/header.jsx
@@ -14,38 +14,39 @@ export default function Header() {
 	return (
 		<header className="bg-main">
 			{/* You can remove "max-w-7xl" to remove buffers on the sides. */}
-			<nav className="mx-auto flex max-w-7xl items-center justify-between p-2 lg:px-8 lg:justify-start" aria-label="Global">
-				<div className="flex lg:flex-1">
-					<Link className="" href={'/'}>
-						<Image className="h-16" src={logoIcon} alt={"Chess Buddy logo image."}/>
+			<nav className="mx-auto flex flex-wrap items-center justify-between p-2 lg:px-8 lg:justify-start" aria-label="Global">
+				<div className="flex justify-center lg:justify-start w-full lg:w-auto mb-4 lg:mb-0">
+					<Link href={'/'}>
+						<Image className="h-16" src={logoIcon} alt={"Chess Buddy logo image."} />
 					</Link>
 				</div>
-				<div className="lg:flex lg:gap-x-12">
+				<div className="flex justify-center lg:justify-start w-full lg:w-auto mb-4 lg:mb-0">
 					<Link
-						className="text-xl font-semibold leading-6 text-white hover:underline hover:underline-offset-8"
+						className="text-xl font-semibold leading-6 text-white hover:underline hover:underline-offset-8 mr-4 lg:mr-12"
 						href="/"
 					>
 						Home
 					</Link>
 					<span className="text-navseparator text-xl font-semibold leading-6">|</span>
 					<Link
-						className="text-xl font-semibold leading-6 text-white hover:underline hover:underline-offset-8"
+						className="text-xl font-semibold leading-6 text-white hover:underline hover:underline-offset-8 mx-4 lg:mx-12"
 						href="/news"
 					>
 						News*
 					</Link>
 					<span className="text-navseparator text-xl font-semibold leading-6">|</span>
 					<Link
-						className="text-xl font-semibold leading-6 text-white hover:underline hover:underline-offset-8"
+						className="text-xl font-semibold leading-6 text-white hover:underline hover:underline-offset-8 ml-4 lg:ml-12"
 						href="/docs"
 					>
 						Docs*
 					</Link>
 				</div>
-				<div className="lg:flex lg:flex-1 lg:justify-end">
-					<LoginLogoutButton/>
+				<div className="flex justify-center lg:justify-end w-full lg:w-auto">
+					<LoginLogoutButton />
 				</div>
 			</nav>
 		</header>
 	)
 }
+
