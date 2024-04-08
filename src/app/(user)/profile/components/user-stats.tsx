11f0312812750cb26946694c981abcb14ea7a154
frigvid <67565373+frigvid@users.noreply.github.com>
2024-04-08 21:58:40 +0200

    feat: Make UserStats take values as props.

--- src/app/(user)/profile/components/user-stats.tsx
+++ src/app/(user)/profile/components/user-stats.tsx
@@ -1,41 +1,62 @@
+"use client";
+
+import {flag, code, name, countries} from "country-emoji"
+import React from "react";
+
+/**
+ * User profile's stat prop types.
+ */
+interface UserStatsProps {
+	elo_rank: number;
+	games_played: number;
+	games_won: number;
+	games_lost: number;
+	games_drawn: number;
+	nationality: string;
+}
+
 /**
  * User profile's stat component.
  *
  * @author frigvid
  * @created 2024-04-03
  */
-export default function UserStats() {
-	
-	const {flag, code, name, countries} = require("country-emoji");
-	
+function UserStats({elo_rank, games_played, games_won, games_lost, games_drawn, nationality}: UserStatsProps) {
 	return (
 		<div className="flex justify-around text-center space-x-4">
 			<div>
 				<h3 className="text-lg font-bold">ELO Rating</h3>
-				<p>1107</p>
+				<p>{elo_rank}</p>
 			</div>
 			<div>
 				<h3 className="text-lg font-bold">Games Played</h3>
-				<p>120</p>
+				<p>{games_played}</p>
 			</div>
 			<div>
 				<h3 className="text-lg font-bold">Wins</h3>
-				<p>89</p>
+				<p>{games_won}</p>
 			</div>
 			<div>
 				<h3 className="text-lg font-bold">Losses</h3>
-				<p>31</p>
+				<p>{games_lost}</p>
 			</div>
 			<div>
 				<h3 className="text-lg font-bold">Draws</h3>
-				<p>31</p>
+				<p>{games_drawn}</p>
 			</div>
 			{/* TODO: If the user does not have a set nationality, hide this. */}
 			<div>
 				<h3 className="text-lg font-bold">Nationality</h3>
 				{/* FIXME: Unicode country codes does not render as an emoji. Might be prudent to switch to SVG. */}
-				<p>{flag("Norway")}</p>
+				<p>{flag(nationality)}</p>
 			</div>
 		</div>
 	);
 }
+
+/**
+ * Memoized user stats component.
+ *
+ * This is to prevent unnecessary re-renders. They were occurring . . . more than I'd have liked, beforehand.
+ */
+export default React.memo(UserStats);
