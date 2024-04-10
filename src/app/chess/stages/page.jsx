ce1e76eef429b4e7c45916dafcb6d3c620897484
frigvid <67565373+frigvid@users.noreply.github.com>
2024-04-10 14:34:50 +0200

    refactor: Normalize file, and make ignored promise void.

--- src/app/chess/stages/page.jsx
+++ src/app/chess/stages/page.jsx
@@ -1,39 +1,37 @@
 "use client";
-import MyModal from '@ui/chess/stages/MyModal'
-import { fetchOpenings } from '@utils/game/get-gamedata';
-import { useEffect, useState } from 'react';
+
+import {fetchOpenings} from "@utils/game/get-gamedata";
+import MyModal from "@ui/chess/stages/MyModal";
+import {useEffect, useState} from "react";
 
 export default function App() {
-
-
-const [opening, setOpening] = useState([]); 
-console.log(opening);
-
-useEffect(() => {
-  async function getOpenings() {
-    const fetchedData = await fetchOpenings();
-    setOpening(fetchedData);
-  }
-
-  getOpenings();
-}, []);
-
-  return (
-    <>
-    <div className='flex justify-center items-center'>
-      <div className='grid grid-cols-4 gap-4'>
-      {opening.map((opening) => {
-          return(
-            <div className='p-4 bg-red-400 rounded-md max-h-32'>
-            <h2>{opening.name}</h2>
-            <p></p>
-            <MyModal title={opening.name} details={opening.desc} id={opening.id}/>
-          </div>
-          )
-        })}
-
-     </div>
-    </div>
-    </>
-  )
+	const [opening, setOpening] = useState([]);
+	console.log(opening);
+	
+	useEffect(() => {
+		async function getOpenings() {
+			const fetchedData = await fetchOpenings();
+			setOpening(fetchedData);
+		}
+		
+		void getOpenings();
+	}, []);
+	
+	return (
+		<>
+			<div className='flex justify-center items-center'>
+				<div className='grid grid-cols-4 gap-4'>
+					{opening.map((opening) => {
+						return (
+							<div key={opening} className='p-4 bg-red-400 rounded-md max-h-32'>
+								<h2>{opening.name}</h2>
+								<p></p>
+								<MyModal title={opening.name} details={opening.desc} id={opening.id}/>
+							</div>
+						)
+					})}
+				</div>
+			</div>
+		</>
+	)
 }
