b365a038219c56fa2b7f023276948c15b27ed5c3
frigvid <67565373+frigvid@users.noreply.github.com>
2024-04-11 15:25:17 +0200

    feat: Make use of FriendsList, and show it only when friends visibility is public.

--- src/app/(user)/profile/[id]/page.tsx
+++ src/app/(user)/profile/[id]/page.tsx
@@ -10,6 +10,7 @@ import React, {useEffect, useState} from "react";
 import {usePathname} from "next/navigation";
 import Avatar from "@mui/material/Avatar";
 import Link from "next/link";
+import FriendList from "@user/profile/components/friend-list";
 
 /**
  * Stub page for user profiles.
@@ -103,14 +104,13 @@ export default function UserProfile() {
 					</div>
 					{/* Body: Friend's list. */}
 					<div className="p-5">
-						<h2 className="border-b-2 border-[#a1887f] pb-1 font-bold">Friends</h2>
-						<div className="mt-2">
-							<p>
-								<span className="underline" title="@omnissiah#1111">Omnissiah</span>
-								<span>, </span>
-								<span className="underline" title="@serbianatlasian#7774">Serbian Atlasian</span>
-							</p>
-						</div>
+						{
+							(data.visibility_friends === true)
+								? <FriendList/>
+								: ((user !== null && staticUserId == user.id)
+										? <FriendList/>
+										: null)
+						}
 					</div>
 				</div>
 			</main>
