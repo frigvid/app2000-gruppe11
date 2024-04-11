a2549f493a2b21fe02aed41ec4f585f6694ee158
frigvid <67565373+frigvid@users.noreply.github.com>
2024-04-11 15:45:34 +0200

    feat: Update FriendList component.

--- src/app/(user)/profile/components/friend-list.jsx
+++ src/app/(user)/profile/components/friend-list.jsx
@@ -7,12 +7,12 @@
 export default function FriendList() {
 	return (
 		<>
-			<h2>Friends</h2>
-			<div>
-				<br/>
+			<h2 className="border-b-2 border-[#a1887f] pb-1 font-bold">Friends</h2>
+			<div className="mt-2">
 				<p>
-					<span title="@strangegiraffe#1184">Gurdian Sprite</span>,
-					<span title="@serbianatlasian#7774">Serbian Atlasian</span>
+					<span className="underline" title="@omnissiah#1111">Omnissiah</span>
+					<span>, </span>
+					<span className="underline" title="@serbianatlasian#7774">Serbian Atlasian</span>
 				</p>
 			</div>
 		</>
