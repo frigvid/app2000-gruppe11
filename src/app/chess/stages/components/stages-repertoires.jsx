4505955d9c1445d5d440faa6019721d18a4d53a3
frigvid <67565373+frigvid@users.noreply.github.com>
2024-04-15 10:45:41 +0200

    feat: Add repertoire realtime updates.

--- src/app/chess/stages/components/stages-repertoires.jsx
+++ src/app/chess/stages/components/stages-repertoires.jsx
@@ -1,12 +1,7 @@
 "use client";
 
-import StagesChessboardThumbnail from "@ui/chess/stages/stages-chessboard-thumbnail";
-import StagesCreateOpeningModal from "@ui/chess/stages/stages-create-opening-modal";
-import ProtectClientContent from "@/app/(auth)/components/protect-client-content";
 import Buffering from "@auth/components/fragment/Buffering";
 import React, {Suspense, useEffect, useState} from "react";
-import StagesModal from "@ui/chess/stages/stages-modal";
-import {fetchOpenings} from "@utils/game/get-gamedata";
 import {createClient} from "@utils/supabase/client";
 import {useTranslation} from "react-i18next";
 
@@ -22,6 +17,71 @@ import {useTranslation} from "react-i18next";
 export default function StagesRepertoires() {
 	const supabase = createClient();
 	const {t} = useTranslation();
+	const [repertoires, setRepertoires] = useState([]);
+	
+	useEffect(() => {
+		const fetchRepertoires = async () => {
+			const {data, error} = await supabase
+				.from('repertoire')
+				.select('id, timestamp, usr, openings');
+			
+			if (error) {
+				console.error("Something went wrong while getting repertoires!", error);
+			} else {
+				setRepertoires(data);
+			}
+		};
+		
+		void fetchRepertoires();
+	}, [supabase]);
+	
+	/**
+	 * Handles DELETE events occurring in table subscription.
+	 *
+	 * @author frigvid
+	 * @created 2024-04-15
+	 * @param payload The row that was deleted.
+	 * @see https://supabase.com/docs/guides/realtime
+	 */
+	const handleDeletes = (payload) => {
+		setRepertoires(prevRepertoires => prevRepertoires.filter(opening => opening.id !== payload.record.id));
+	}
+	
+	/**
+	 * Handles INSERT events occurring in table subscription.
+	 *
+	 * @author frigvid
+	 * @created 2024-04-15
+	 * @param payload The row that was inserted.
+	 * @see https://supabase.com/docs/guides/realtime
+	 */
+	const handleInserts = (payload) => {
+		setRepertoires(prevRepertoires => [...prevRepertoires, payload.record]);
+	}
+	
+	/**
+	 * Subscription to listen to DELETE events.
+	 *
+	 * @author frigvid, supabase
+	 * @created 2024-04-15
+	 * @see https://supabase.com/docs/guides/realtime
+	 */
+	supabase
+		.channel('openings')
+		.on('postgres_changes', { event: 'DELETE', schema: 'public', table: 'repertoire' }, handleDeletes)
+		.subscribe();
+	
+	/**
+	 * Subscription to listen to INSERT events.
+	 *
+	 * @author frigvid, supabase
+	 * @created 2024-04-15
+	 * @see https://supabase.com/docs/guides/realtime
+	 */
+	supabase
+		.channel('openings')
+		.on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'repertoire' }, handleInserts)
+		.subscribe();
 	
 	return (
 		<>
@@ -30,7 +90,28 @@ export default function StagesRepertoires() {
 					<h2 className="text-center font-semibold text-3xl">Your repertoires</h2>
 					<section id="repertoireList">
 						{
-						
+							(repertoires === null)
+								? <p></p>
+								: (
+									repertoires.map((repertoire) => {
+										return (
+											<div
+												key={(repertoire.title + repertoire.description + repertoire.id)}
+												className='p-4 bg-[#976646] rounded-md w-64 h-64 flex flex-col justify-between items-center text-white'
+											>
+												<h2 className='font-semibold'>{repertoire.title}</h2>
+												{/*
+												<div>
+													<StagesChessboardThumbnail pgn={repertoire.pgn}/>
+												</div>
+												<div>
+													<StagesModal created_by={repertoire.created_by} title={repertoire.title} details={repertoire.description} id={repertoire.id} pgn={repertoire.pgn}/>
+												</div>
+												*/}
+											</div>
+										)
+									})
+								)
 						}
 					</section>
 				</div>
