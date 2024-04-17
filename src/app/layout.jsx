10b4825904de2a3d94ea54e72fa5b1dd2ff1ebdf
frigvid <67565373+frigvid@users.noreply.github.com>
2024-04-17 19:27:35 +0200

    refactor: Move header and footer components to shared/components/base.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import {Inter} from "next/font/google";
-import Header from "@ui/header";
-import Footer from "@ui/footer";
+import Header from "@/app/shared/components/base/header";
+import Footer from "@/app/shared/components/base/footer";
 import "@/app/globals.css";
 /**
  * Do not remove, is in use when starting the translation.
