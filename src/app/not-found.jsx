2e0dd21b4ad3638289f3f0bf16c50f9134b0d560
frigvid <67565373+frigvid@users.noreply.github.com>
2024-04-19 00:05:50 +0200

    fix: Missing header and footer in default 404 override.

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,17 +1,40 @@
+"use client";
+
+import withI18next from "@shared/components/lang/with-i18next";
+import Footer from "@shared/components/base/footer";
+import Header from "@shared/components/base/header";
+
 /**
  * A custom 404 page for the application, to avoid the
  * default 404 page from pushing the footer out of the
  * viewport.
  *
+ * Since this is a default page, and 'shared' is not a
+ * route, the header and footer component are included
+ * manually.
+ *
  * @author frigvid
  * @created 2024-01-19
  */
-export default function NotFound() {
+function NotFound() {
 	return (
-		<div className="flex items-center justify-center">
-			<h1 className="text-3xl font-medium mr-5 p-0">404</h1>
-			<span>|</span>
-			<h2 className="text-base font-normal m-0 pl-5">This page could not be found.</h2>
-		</div>
+		<>
+			<Header/>
+				<div className="flex items-center justify-center">
+					<h1 className="text-3xl font-medium mr-5 p-0">404</h1>
+					<span>|</span>
+					<h2 className="text-base font-normal m-0 pl-5">This page could not be found.</h2>
+				</div>
+			<Footer/>
+		</>
 	);
 }
+
+/**
+ * Export with extra i18next support,
+ * to avoid hydration errors.
+ *
+ * @author frigvid
+ * @created 2024-04-19
+ */
+export default withI18next(NotFound);
