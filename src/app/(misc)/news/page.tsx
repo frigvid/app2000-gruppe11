3a9441b569443e43a99fceaae79301556758ae39
frigvid <67565373+frigvid@users.noreply.github.com>
2024-04-20 20:45:27 +0200

    feat: Add ability to delete news.

--- src/app/(misc)/news/page.tsx
+++ src/app/(misc)/news/page.tsx
@@ -7,7 +7,9 @@ import IconButton from "@mui/material/IconButton";
 import LockIcon from "@mui/icons-material/Lock";
 import {useTranslation} from "react-i18next";
 import Tooltip from "@mui/material/Tooltip";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
+import NewsModal from "@misc/news/components/news-modal";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 /**
  * News page.
@@ -128,11 +130,43 @@ export default function News() {
 		}
 	}, [isAdmin, supabase]);
 	
+	/**
+	 * Deletes a news item.
+	 *
+	 * @author frigvid
+	 * @created 2024-04-20
+	 * @param newsItemId The ID of the news item to delete.
+	 */
+	function deleteNews(newsItemId: any) {
+		return (
+			<ConfirmBeforeAction
+				confirmMessage={`Are you sure you want to delete this news item?`}
+				onConfirm={async () => {
+					const {error} = await supabase
+						.from('news')
+						.delete()
+						.eq('id', newsItemId);
+					
+					if (error) {
+						console.error("Error deleting news item!", error);
+					}
+				}}
+			>
+				<Tooltip title="Delete news item?">
+					<IconButton>
+						<DeleteIcon color="error"/>
+					</IconButton>
+				</Tooltip>
+			</ConfirmBeforeAction>
+		)
+	}
+	
 	return (
 		<main className="grid grid-cols-1 grid-rows-[auto,1fr,auto] m-4">
 			<h1 className="self-start text-4xl font-bold text-center">
 				{t("news.title")}
 			</h1>
+			<NewsModal isAuthoring={true}/>
 			<div className="grid grid-cols-1 gap-4 place-self-center md:w-[30rem] lg:w-[35rem] align-middle">
 				{
 					news.map((newsItem) => (
@@ -140,8 +174,8 @@ export default function News() {
 							key={newsItem.id}
 							className={`relative bg-white ${newsItem.is_published ? null : "border-2 border-red-500"} p-4 rounded-lg shadow-md h-auto lg:min-h-[10rem]`}
 						>
-							<div className="absolute top-0 right-0 m-2">
-								{/* Edit button goes here. */}
+							<div className="absolute top-0 right-0 m-2 space-y-2 flex flex-col">
+								<NewsModal isAuthoring={false}/>
 								{
 									(user && isAdmin)
 										? (
@@ -160,16 +194,21 @@ export default function News() {
 													}
 												}}
 											>
-												<IconButton className="bg-white">
-													{
-														newsItem.is_published
-															? <LockOpenIcon className="text-green-500"/>
-															: <LockIcon className="text-red-500"/>
-													}
-												</IconButton>
+												<Tooltip title={`Do you want to ${newsItem.is_published ? t("news.publishing.unpublish") : t("news.publishing.publish")} this?`}>
+													<IconButton className="bg-white">
+														{
+															newsItem.is_published
+																? <LockOpenIcon className="text-green-500"/>
+																: <LockIcon className="text-red-500"/>
+														}
+													</IconButton>
+												</Tooltip>
 											</ConfirmBeforeAction>
 										)
 										: null
+								}
+								{
+									deleteNews(newsItem.id)
 								}
 							</div>
 							{/* TODO: Implement using the user's profile.display_name instead of UUID. */}
@@ -187,7 +226,7 @@ export default function News() {
 									</h2>
 								</Tooltip>
 								<Tooltip title={t("news.item.summary")}>
-									<p className="italic text-center lg:text-left">
+									<p className="italic text-center lg:text-left mr-6">
 										{newsItem.summary}
 									</p>
 								</Tooltip>
@@ -196,7 +235,7 @@ export default function News() {
 									 * but is included as an example for now.
 									 */
 								}
-								<p>
+								<p className="mr-6">
 									{newsItem.content}
 								</p>
 							</div>
